afcba91703667bc5b175690c616c8fd2c4d88c9d
Ford <ford_wang@airyzone.com>
2017-06-01 11:34:09 +0800

    ver 0.0531.1

---
+++ BLE_TEST/Framework/BLESDK.framework/Headers/BLESDK.h
@@ -0,0 +1,20 @@
+//
+//  BLESDK.h
+//  BLESDK
+//
+//  Created by Rusty Huang on 2017/5/23.
+//  Copyright © 2017年 Rusty Huang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BLESDK.
+FOUNDATION_EXPORT double BLESDKVersionNumber;
+
+//! Project version string for BLESDK.
+FOUNDATION_EXPORT const unsigned char BLESDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BLESDK/PublicHeader.h>
+
+#import "mBeaconConstant.h"
+#import "mBeacon.h"
